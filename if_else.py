e215f9e00c11f20c681b15a74b3559e48d6a1627
MLKODER <72186520+MLKODER@users.noreply.github.com>
2020-10-02 15:42:40 +0530

    Just a suggestion for Improved version of code
    I just used the " single line if and else statement " to reduce your code length and change into a short length.
    
    The format of the if and else was : 
    
    value_when_true if condition else value_when_false
    
    for more information on the above code, you can visit this link: https://stackoverflow.com/questions/2802726/putting-a-simple-if-then-else-statement-on-one-line
    
    I hope you will like the code !!!

--- if_else.py
+++ if_else.py
@@ -1,23 +1,14 @@
 age = 23
 
-if age >= 18:
-    print("You are adult and you can buy alcohol")
-else:
-    print('you are to young to buy alcohol')
+print("You are adult and you can buy alcohol") if age >= 18 else print('you are to young to buy alcohol')
 
 isDrunk = False
 
-if age >= 18 and not isDrunk:
-    print('You are adult and you can buy alcohol')
-else:
-    print('sorry, we cannot sell you alcohol')
+print('You are adult and you can buy alcohol') if age >= 18 and not isDrunk else print('sorry, we cannot sell you alcohol')
 
 isRestrictedArea = False
 
-if age >= 18 and not isDrunk and not isRestrictedArea:
-    print('you can buy alcohol')
-else:
-    print('we cannot sell you alcohol')
+print('you can buy alcohol') if age >= 18 and not isDrunk and not isRestrictedArea else print('we cannot sell you alcohol')
 
 #zadanie 1
 
@@ -27,10 +18,7 @@ min_shares = 100
 num_of_likes = 1300
 num_of_shares = 70
 
-if num_of_likes >= min_likes and num_of_shares >= min_shares:
-    print('discount 10%')
-else:
-    print('not enough shares and likes')
+print('discount 10%') if num_of_likes >= min_likes and num_of_shares >= min_shares else print('not enough shares and likes')
 
 #zadanie 2
 
@@ -38,10 +26,7 @@ isPizzaOrdered = True
 isBigDrinkOrderes = False
 isWeekend = False
 
-if not isWeekend and (isBigDrinkOrderes or isPizzaOrdered):
-    print('free burger!!!')
-else:
-    print('Buy pizza or big drink during work days to get free burger')
+print('free burger!!!') if not isWeekend and (isBigDrinkOrderes or isPizzaOrdered) else print('Buy pizza or big drink during work days to get free burger')
 
 #zadanie 3
 
@@ -49,7 +34,4 @@ diskSize = 140
 diskSizeUsed = 119
 fileSize = 21
 
-if fileSize <= diskSize - diskSizeUsed:
-    print('You can download file')
-else:
-    print('not enough space on disk')
+print('You can download file') if fileSize <= diskSize - diskSizeUsed else print('not enough space on disk')
